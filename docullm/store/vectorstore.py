9385d97060b752cfb843a86fbbfa3bfa74fd5422
mayukh18 <mayukhbhattacharyya18@gmail.com>
2024-03-06 21:17:02 -0500

    updated vectorstore

--- docullm/store/vectorstore.py
+++ docullm/store/vectorstore.py
@@ -3,15 +3,17 @@ from docullm.utils import getVectorStore, getEmbeddings
 
 
 class VectorStore:
-    def __init__(self, db_name: str, embedding_name: str, persist_dir: str, client_settings) -> None:
+    def __init__(self, collection_name: str, db_type: str, embedding_type: str, persist_dir: str) -> None:
         self.persist_dir = persist_dir
-        self.embeddings = getEmbeddings(embedding_name)
-        self.db = getVectorStore(db_name)
+        self.collection_name = collection_name
+        self.embeddings = getEmbeddings(embedding_type)
+        self.db = getVectorStore(db_type)
         
+    def load(self):
         self.vectorstore = self.db(
+            collection_name=self.collection_name,
             embedding_function=self.embeddings(),
-            client_settings=client_settings,
-            persist_directory=persist_dir,
+            persist_directory=self.persist_dir,
         )
         
     def ingest(self, documents: list) -> None:
@@ -21,9 +23,6 @@ class VectorStore:
                                     embedding=self.embeddings(),
                                     persist_directory=self.persist_dir)
         self.vectorstore.persist()
-        
-    def load(self):
-        self.vectorstore = self.db(self.persist_dir, self.embeddings())
     
     def getRetriever(self, k):
         retriever = self.vectorstore.as_retriever(search_kwargs={"k": k})
